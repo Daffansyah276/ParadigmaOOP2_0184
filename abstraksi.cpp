b717b240b09cdd87d24ab9bf408d1814436938e7
Daffansyah276 <daffansyaharya@gmail.com>
2025-06-02 15:12:04 +0700

    membuat class abstraksi

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class AbstraksiClass {
+    private: string x, y;
+
+    public:
+        // Method untuk mengisi nilai
+        // Private member
+        void setXY(string a, string b){
+            x = a;
+            y = b;
+        }
+        // menampilkan nilai
+        void display(){
+            cout << "x = " << x << endl;
+            cout << "y = " << y << endl;
+        }
+};
